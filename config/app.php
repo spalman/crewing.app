52f7220414ea99e67b176f07de4b81dc960efaff
spalman <spalman01@gmail.com>
2016-07-29 21:45:45 +0300

    remove debug panel

--- config/app.php
+++ config/app.php
@@ -156,7 +156,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
-        Barryvdh\Debugbar\ServiceProvider::class,
     ],
 
     /*
